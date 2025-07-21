dc59fe0064d405034a42907a9f9a18ea7ce362c8
Nilesh1506 <36773937+Nilesh1506@users.noreply.github.com>
2025-07-21 17:48:57 +0530

    Update HelloWorld.java
    Everything

--- javaapp/src/test/java/javaapp/HelloWorld.java
+++ javaapp/src/test/java/javaapp/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Nilesh Yadav");
+		System.out.println("Hello Nilesh Yadav everything is fine");
 		System.out.println("today is raining to Heavy here");
 
 		System.out.println("Hello Nilesh Yadav what are doing");
